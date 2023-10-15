b692f66551c4a10dce6f20574c12c0b575b1c715
Ojay16 <juliusjeff16@gmail.com>
2023-10-14 19:27:10 -0700

    updated

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class manages amenity objects"""
+
+    name = ""
